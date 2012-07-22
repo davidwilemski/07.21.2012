e665244ba7d1ae1d3f809f4cfac0ac1eb98da95d
David Wilemski <david@davidwilemski.com>
2012-07-22 03:17:09 -0700

    typo

--- web/demo1/push_client.py
+++ web/demo1/push_client.py
@@ -6,5 +6,5 @@ def send_push(device_token, appname, domain, callback):
     d = {'appname': appname, 'domain': domain, 'device_token': device_token}
 
     request = httpclient.HTTPRequest('http://23.21.143.75:8888/push', method='POST', body=escape.json_encode(d))
-    http = httpclient.AsyncHTTPClient(requst)
+    http = httpclient.AsyncHTTPClient(request)
     yield gen.Task(http.fetch)
