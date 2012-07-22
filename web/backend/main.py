5690348a5200cf2a102f3c774abd3821c07f83ce
David Wilemski <david@davidwilemski.com>
2012-07-22 01:34:24 -0700

    add missing argument

--- web/backend/main.py
+++ web/backend/main.py
@@ -34,5 +34,5 @@ def main():
 
 if __name__ == '__main__':
     tornado.options.parse_command_line()
-    mongoengine.connect(options.mongo)
+    mongoengine.connect('greylock', options.mongo)
     main()
