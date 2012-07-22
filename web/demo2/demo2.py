7973097635eca15e4310ccc2b0041728d8526193
David Wilemski <david@davidwilemski.com>
2012-07-22 07:17:08 -0700

    add demo 2

---
+++ web/demo2/demo2.py
@@ -0,0 +1,34 @@
+import tornado.ioloop
+import tornado.web
+import os
+from tornado import gen
+import push_client
+import logging
+
+class MainHandler(tornado.web.RequestHandler):
+    def get(self):
+        logger = logging.getLogger('demo1')
+        logger.info("at /")
+        self.render("index.html")
+
+class PushHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    @tornado.gen.engine
+    def get(self, device_token):
+        yield gen.Task(push_client.send_push, device_token, 'demo2', 'http://23.21.143.75:8899')
+        self.finish('ok')
+
+
+settings = {
+    'template_path': os.path.join(os.path.dirname(__file__), "templates"),
+    
+}
+application = tornado.web.Application([
+    (r"/", MainHandler),
+    (r"/sendpush/([a-zA-Z0-9]+)", PushHandler),
+], **settings)
+
+if __name__ == "__main__":
+    application.listen(8899)
+    tornado.ioloop.IOLoop.instance().start()
+
